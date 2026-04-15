2a4dd0c5be22a26d1d1ab16897600462cabb8e5e
Meowwy <koleckar551@gmail.com>
2026-04-15 08:03:34 +0200

    fix vite config - enable svelte plugin

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  /*plugins: [svelte()],*/
+  plugins: [svelte()],
   base: '/skoda-incidents-public/'
 })
